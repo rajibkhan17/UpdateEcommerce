14bf01d8fa9720779ebcd948e45a75e29065ccb9
rajibkhan17 <rajibkhan8189@gmail.com>
2021-08-19 11:04:44 +0600

    register form success

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from 'react'
 
 const Register = () => {
+    const [email, setEmail] = useState('')
+
+    const handleSubmit = () => {
+
+    }
+
+
+    const RegisterForm = () =>  (
+    <form onSubmit={handleSubmit}>
+        <input type="email" className="form-control" name="" value={email} onChange={e => setEmail(e.target.value)} autoFocus />
+        <button type="submit" className="btn btn-info mt-3">Register</button>
+    </form>)
+    
     return (
-        <div>
-
-           <p>new user register first</p>
-
+        <div className="container p-5">
+           <div className="row">
+               <div className="col-md-6 offset-md-3">
+                   <h4>Register</h4>
+                   {RegisterForm()}
+               </div>
+           </div>
         </div>
     );
 };
