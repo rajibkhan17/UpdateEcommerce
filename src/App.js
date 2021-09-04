83a5945868ed58264a19d9972ac8735d0528bbde
rajibkhan17 <rajibkhan8189@gmail.com>
2021-09-04 16:00:44 +0600

    chake firebase auth state for { token, email, name }

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
@@ -7,9 +7,40 @@ import Header from "./components/nav/Header"
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
 import RegisterComplete from "./pages/auth/RegisterComplete";
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+
+
+
 
 
 const App = () => {
+
+   const dispatch = useDispatch()
+
+  //to chake firebase auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if(user) {
+        const idTokenResult = await user.getIdTokenResult()
+
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            token: idTokenResult,
+            email: user.email,
+            name: "rajib",
+          },
+        });
+      }
+    });
+
+    //clean up
+    return () => unsubscribe();
+
+  }, [])
+
+
   return (
     <>
     <Header />
