e556ff90c0b93800885a1e64173962642054295c
rajibkhan17 <rajibkhan8189@gmail.com>
2021-09-04 15:17:15 +0600

    redux initialize

--- src/index.js
+++ src/index.js
@@ -6,18 +6,36 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import "antd/dist/antd.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from "redux-devtools-extension"
+
+import rootReducer from './reducers';
+
+
+//store
+const store = createStore(rootReducer, composeWithDevTools())
+
 
 ReactDOM.render(
-  //<React.StrictMode>
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>,
+  <React.StrictMode>
+<Provider store={store} >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    </Provider>
     
-  //</React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>,
+  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+
+
